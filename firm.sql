c9b858a227282b05e9d26b8edb48bf7fb2bf045d
ShreyaShradha <153638626+ShreyaShradha@users.noreply.github.com>
2023-12-26 21:53:15 +0530

    Add files via upload

---
+++ firm.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2023 at 04:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `payroll`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `firm`
+--
+
+CREATE TABLE `firm` (
+  `sno` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `firm`
+--
+
+INSERT INTO `firm` (`sno`, `name`) VALUES
+(1, 'ABCD pvt ltd'),
+(2, 'City Center'),
+(3, 'Sec 4 , Bokaro'),
+(4, 'Jharkhand');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `firm`
+--
+ALTER TABLE `firm`
+  ADD PRIMARY KEY (`sno`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
